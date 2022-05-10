523c22827326597cba00a61506cf29a171c3c90f
moshiur25524 <moshiur.rahman25524@gmail.com>
2022-05-11 01:31:50 +0600

    Mongo db is connected with Server

--- index.js
+++ index.js
@@ -1,17 +1,32 @@
 const express = require('express');
 const app = express()
 const cors = require('cors');
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const port = process.env.PORT || 5000;
 
 // middleWare
-
 app.use(cors())
 app.use(express.json())
 
-app.get('/' , (req, res)=>{
+// user: BookHouse
+// Password: cLkRbyB1qzkFgK7c
+
+
+
+const uri = "mongodb+srv://<username>:<password>@cluster0.agg0z.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+client.connect(err => {
+    const collection = client.db("test").collection("devices");
+    console.log('Mongo db connected');
+    // perform actions on the collection object
+    client.close();
+});
+
+
+app.get('/', (req, res) => {
     res.send('Server for Assignment')
 })
 
-app.listen(port, ()=>{
+app.listen(port, () => {
     console.log('Listening', port);
 })
